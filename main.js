899cf8cc932b21d0a2be6ec87a6d26a51bf13383
Manuel Scholz <manuel.serrano943@gmail.com>
2025-02-03 21:50:18 +0100

    Feat: Toggle mapa/listado

---
+++ main.js
@@ -0,0 +1,15 @@
+// Global
+const listElement = document.getElementById("list");
+const mapElement = document.getElementById("map");
+
+const toggleMapElement = document.getElementById("toggle-map");
+toggleMapElement.addEventListener("click", () => {
+    listElement.classList.add("hidden");
+    mapElement.classList.add("show");
+})
+
+const toggleListElement = document.getElementById("toggle-list");
+toggleListElement.addEventListener("click", () => {
+    listElement.classList.remove("hidden");
+    mapElement.classList.remove("show");
+})
