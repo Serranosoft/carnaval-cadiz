86dd0d0409ba8a2836aaa9f19b50d03fe25a1f42
Manuel Scholz <manuel.serrano943@gmail.com>
2025-02-12 20:27:50 +0100

    Feat: Datos dinamicos extraidos del JSON para el modal de detalle de horarios

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ init();
 const boxTemplate = document.getElementById("list-element-template");
 const list = document.querySelector(".list .wrapper");
 const modal = document.getElementById("modal");
+const modalItemTemplate = document.querySelector(".modal-item-template");
+const modalItemRowTemplate = document.querySelector(".modal-item-row-template");
 
 function init() {
     renderUpdate();
@@ -40,12 +42,54 @@ async function renderList() {
         nombre.textContent = agrupacion.nombre;
         apodo.textContent = agrupacion.apodo;
         button.textContent = "Ver horarios";
-        button.addEventListener("click", () => {
-            modal.classList.add("show");
-        })
+        button.addEventListener("click", () => fillModal(agrupacion))
 
         list.appendChild(boxElement);
     })
+}
+
+
+async function fillModal(agrupacion) {
+    console.log(agrupacion);
+    // Traer los datos del pulsado
+
+    const modalContent = document.querySelector("#modal .content");
+    const modalTitle = document.querySelector(".modal-title");
+    modalTitle.textContent = agrupacion.nombre;
+
+    
+    agrupacion.actuaciones.forEach((actuacion) => {
+        const modalItemRef = modalItemTemplate.content.cloneNode(true);
+        const modalItem = modalItemRef.querySelector(".item");
+        console.log(modalItem);
+        const date = modalItem.querySelector(".modal-item-date");
+        date.textContent = actuacion.fecha;
+
+        const modalInfo = modalItemRef.querySelector(".info");
+
+        actuacion.metadata.forEach((metadata) => {
+            const modalItemRowRef = modalItemRowTemplate.content.cloneNode(true);
+            const modalItemRow = modalItemRowRef.querySelector(".row");
+
+            const modalItemHour = modalItemRow.querySelector(".modal-item-hour");
+            modalItemHour.textContent = metadata.hora;
+
+            const modalItemStreet = modalItemRow.querySelector(".modal-item-street");
+            modalItemStreet.textContent = metadata.lugar;
+            
+            const modalItemButton = modalItemRow.querySelector(".button");
+            modalItemButton.href = `/map.html?calle=${metadata.lugar.split(" ").join("+")}`;
+
+
+            modalInfo.appendChild(modalItemRow);
+        })
+
+        modalItem.appendChild(modalInfo);
+        modalContent.appendChild(modalItem);
+    })
+    
+    
+    modal.classList.add("show");
 }
 
 async function addModalEvents() {
